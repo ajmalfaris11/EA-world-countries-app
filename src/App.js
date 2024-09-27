93ba8ed2b509f62746d897762f14c6804d3913cd
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-27 09:03:20 +0530

    header designed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './App.css'
 
 const App = () => {
   // update the countries values
@@ -9,21 +10,21 @@ const App = () => {
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await axios.get('https://restcountries.com/v3.1/all');
+        const response = await axios.get("https://restcountries.com/v3.1/all");
         setCountries(response.data);
       } catch (error) {
-        console.error('Error fetching countries:', error);
+        console.error("Error fetching countries:", error);
       }
     };
 
     fetchCountries();
   }, []);
 
-  return (
-    <div>
-    
-    </div>
-  );
+  return <div className="world-container">
+    {/* header */}
+    <h1>Countries of the world</h1>
+
+  </div>;
 };
 
 export default App;
