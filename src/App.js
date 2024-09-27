c3195d09b6c345daf9c9f37b6653b62a988b0501
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-27 08:51:56 +0530

    Implement country data fetching using axios and useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  // update the countries values
+  const [countries, setCountries] = useState([]);
+
+  // fetch the data while loading the app with help of useEffect hook
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        setCountries(response.data);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+    
     </div>
   );
-}
+};
 
 export default App;
